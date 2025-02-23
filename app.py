49594d0755236a6254929d81e5c34efd01da9773
Surendhar1555 <surendhar22td0723@svcet.ac.in>
2025-02-23 23:25:57 +0530

    Mini peoject

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, this is the home page'
-
-@app.route('/welcome/<name>')
-def welcome(name):
-    return render_template('index.html', name=name)
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app2.py
+++ app2.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, request
-from forms import NameForm
-
+from flask import Flask, render_template
+from form import NameForm
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'  # Change this to a real secret key
+app.config['SECRET_KEY'] = '1234'
 
-@app.route('/form', methods=['GET', 'POST'])
+@app.route('/form', methods=['GET','POST'])
 def form():
     form = NameForm()
     if form.validate_on_submit():
-        return f"Hello, {form.name.data}!"
+        return f"Hello,{form.name.data}!"
     return render_template('form.html', form=form)
 
-if __name__ == '__main__':
+@app.errorhandler(404)
+def not_found(error):
+    return "Internal Server Error", 500
+
+if __name__== 'main_':
     app.run(debug=True)
